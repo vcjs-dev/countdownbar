024ae57b11e3d20228eb92d153c838bb49d495db
wenqing <wenqing@kerrylan.com>
2023-08-16 14:49:36 +0800

    chore: initial pkg

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { svg4VuePlugin } from 'vite-plugin-svg4vue'
+
+import { resolve } from 'node:path'
+
+const isBuildLib = () => {
+  return process.env.BUILD_TYPE === 'lib'
+}
+
+const outDir = isBuildLib() ? 'lib' : 'dist'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/countdownbar/',
+  plugins: [
+    vue(),
+    svg4VuePlugin({
+      assetsDirName: false
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: isBuildLib()
+    ? {
+        outDir,
+        target: 'es2015',
+        lib: {
+          // Could also be a dictionary or array of multiple entry points
+          entry: resolve(__dirname, 'src/lib/main.ts'),
+          name: 'Countdownbar',
+          // the proper extensions will be added
+          fileName: 'countdownbar',
+          formats: ['es', 'cjs', 'umd']
+        },
+        rollupOptions: {
+          external: ['fourdom']
+        },
+        copyPublicDir: false,
+        cssTarget: ['chrome35']
+      }
+    : { outDir }
+})
